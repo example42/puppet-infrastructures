ab8413535f3faf8c3f55af2a11ea94aee525db28
root <root@puppet.example42.com>
2010-04-04 17:43:02 +0200

    Added Mailscanner toaster to use with plain "puppet" command

---
+++ toasters/mailscanner.pp
@@ -0,0 +1,30 @@
+# Postfix + Mysql + Dovecot + MailScanner + Mailwatch + Clamav + SpamAssassing
+# Example42 toaster
+
+# Tested on Centos5/RedHat5 
+
+# Customize these settings
+$postfix_mysqluser = "postfix"
+$postfix_mysqlpassword = "example42"
+$postfix_mysqlhost = "localhost"
+$postfix_mysqldbname = "postfix"
+$postfix_mynetworks = "10.42.0.0/16, 127.0.0.1/32"
+
+$mailscanner_mysqluser = "mailwatch"
+$mailscanner_mysqlpassword = "example42"
+$mailscanner_mysqlhost = "localhost"
+$mailscanner_mysqldbname = "mailscanner"
+
+$mailscanner_adminuser = "admin"
+$mailscanner_adminpassword = "admin"
+
+
+include mysql
+include postfix::mysql
+include postfix::postfixadmin
+include dovecot::mysql
+
+include mailscanner::mailwatch
+include clamav
+clamav::instance { "mailscanner": }
+
