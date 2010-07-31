4b1a46f90dc318a7ba2a7ee6923f9cd7e467d7ac
root <root@puppet.example42.com>
2010-07-31 16:43:12 +0200

    Cleanups on puppet toaster

--- toasters/puppet/init.pp
+++ toasters/puppet/init.pp
@@ -5,10 +5,12 @@
 Exec { path => "/bin:/sbin:/usr/bin:/usr/sbin" }
 import "common"
 
+# USER VARIABLES
+# Define here some variables needed by the used modules
+# Refer to the modules' documentation for available options
+
 # The name of the puppet master
-# Note: For the toaster setup of the same PuppetMaster you can leave its $fqdn fact BUT 
-# remember to define it with the Puppet Masters' actual fqdn in your Puppet configurations for the clients
-$puppet_server = "$fqdn" 
+# $puppet_server = "$fqdn" 
 
 # Email address for root's mail forwading
 $root_email = "root"
@@ -16,18 +18,16 @@ $root_email = "root"
 $smtp_server = "localhost"
 
 # Access lists for Puppetmaster access (can be an array)
-$puppet_allow = ["*.example42.com",  "10.42.42.0" ]
-
-# Toaster components
-# Choose the Puppet Master setup you want (only one!)
+# $puppet_allow = [ "*.example42.com" , "10.42.42.0" ]
 
 # Define if you want to use a node tool (with or without external nodes support)
 # Possible values are "foreman" or "dashboard". Default is no tool.
-$puppet_nodetool = "dashboard"
+# $puppet_nodetool = "dashboard"
+# $dashboard_db = "mysql"
 
 # Define if you want to enable external nodes support (you define nodes via the tools' web interface and not in Puppet language)
 # Note that if you enable external nodes support you MUST define a $puppet_nodetool
-$external_nodes = "no"
+# $puppet_externalnodes = "no"
 
+# Include the puppet::master class to setup the Puppet Server
 include puppet::master
-
