07ac4a25d3437affa1591a8953fe85159ee0a93f
root <root@puppet.example42.com>
2010-12-27 20:01:35 +0100

    Puppetmaster toaster checkup

--- toasters/puppet/init.pp
+++ toasters/puppet/init.pp
@@ -10,8 +10,11 @@ import "common"
 # Refer to the modules' documentation for available options
 
 # The name of the puppet master
-# $puppet_server = "$fqdn" 
-$puppet_server="puppet.example42.com"
+$puppet_server = "$fqdn" 
+# $puppet_server="puppet.example42.com"
+
+# Puppet server classes are autoloaded if puppet_server is $fqdn or
+$puppet_server_local == true
 
 # Email address for root's mail forwading
 $root_email = "root"
@@ -23,22 +26,21 @@ $puppet_allow = [ "*.example42.com" , "10.42.42.0" ]
 
 # Define if you want to use a node tool (with or without external nodes support)
 # Possible values are "foreman" or "dashboard". Default is no tool.
-# $puppet_nodetool = "dashboard"
+$puppet_nodetool = "dashboard"
+# $puppet_nodetool = "foreman"
+
 # $dashboard_db = "mysql"
 
 # Define if you want to enable external nodes support (you define nodes via the tools' web interface and not in Puppet language)
 # Note that if you enable external nodes support you MUST define a $puppet_nodetool
 # $puppet_externalnodes = "no"
 
-    $puppet_nodetool = "dashboard"
-    # $puppet_nodetool = "foreman"
-    # $puppet_externalnodes = "no"
-    $dashboard_db = "mysql"
-    $puppet_storeconfigs = "yes"
-    $puppet_db = "mysql"
-    $puppet_db_server = "localhost"
-    $puppet_db_user = "puppet"
-    $puppet_db_password = "mys3cr3tp4ss0rd"
+# Define if you want to activate Storeconfigs. If yes, mysql backend is reccomended
+$puppet_storeconfigs = "yes"
+$puppet_db = "mysql"
+$puppet_db_server = "localhost"
+$puppet_db_user = "puppet"
+$puppet_db_password = "mys3cr3tp4ss0rd"
 
-# Include the puppet::master class to setup the Puppet Server
+# And after all these variable settings we just
 include puppet
